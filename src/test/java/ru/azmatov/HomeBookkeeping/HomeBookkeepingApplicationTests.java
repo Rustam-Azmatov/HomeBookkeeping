a1d89c815ea73fc2847a34fb1d610765ebdf05b8
Rustam-Azmatov <azmatov-rustam@yandex.ru>
2021-06-19 16:11:38 +0500

    prepare project

---
+++ src/test/java/ru/azmatov/HomeBookkeeping/HomeBookkeepingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.azmatov.HomeBookkeeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBookkeepingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
